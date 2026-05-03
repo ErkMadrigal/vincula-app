bfc2b6365aa1a295f6cb5cd8e7557e115be63205
Erk <erick.madfl@gmail.com>
2026-05-03 16:52:10 -0600

    se crea la LP

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.vincula.app',
-  appName: 'Vincúla',
+  appId: 'com.vincula365.app',
+  appName: 'Vincúla365',
   webDir: 'www',
   android: {
     allowMixedContent: true,
